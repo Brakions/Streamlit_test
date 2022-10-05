2b100d4e8871482ab88a6d9a8e8d5547fad00c9c
Brakions <Brakions@hotmail.com>
2022-10-05 16:58:53 -0500

    b

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-from client import lifexp
+from client import lifexp,lifexpe,lifexpa,lifexpaf,lifexpo
 
 def main():
     menu = ["Life Expectancy","Dataset"]
@@ -12,8 +12,16 @@ def main():
 
     if choice == "Life Expectancy":
             st.subheader("Life Expectancy")
-            if st.checkbox("Life Expectancy"):
+            if st.checkbox("America"):
                 st.write(lifexp())
+            if st.checkbox("Europa"):
+                st.write(lifexpe())
+            if st.checkbox("Asia"):
+                st.write(lifexpa())
+            if st.checkbox("Africa"):
+                st.write(lifexpaf())
+            if st.checkbox("Oceania"):
+                st.write(lifexpo())
     else :
             
             st.subheader("Dataset")
--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 df=pd.read_csv("life_expectancy.csv")
 fig=go.Figure()
 
+#Life Expectancy in America
 def lifexp():
     fig=go.Figure()
     fig.update_layout(
@@ -21,9 +22,9 @@ def lifexp():
     fig.add_trace(
                     go.Scatter(
                         x=df["Year"],
-                        name="Peru",
-                        y=df["Peru"],
-                        line = dict(color='red')
+                        name="U.S.A",
+                        y=df["United States"],
+                        line = dict(color='black')
                     )
             )
     fig.add_trace(
@@ -47,20 +48,259 @@ def lifexp():
     fig.add_trace(
                     go.Scatter(
                         x=df["Year"],
-                        name="Alemania",
-                        y=df["Germany"],
-                        line = dict(color='black')
+                        name="Peru",
+                        y=df["Peru"],
+                        line = dict(color='red')
                     )
 
     )
     fig.add_trace(
                     go.Scatter(
                         x=df["Year"],
-                        name="China",
-                        y=df["China"],
+                        name="Mexico",
+                        y=df["Mexico"],
                         line = dict(color='green')
                     )
 
     )
         
     return fig
+#Life Expectancy in Europa
+def lifexpe():
+    fige=go.Figure()
+    fige.update_layout(
+            title={
+                "text":"LifeExpectancy",
+                "x" : 0.5,
+                "xanchor":"center"
+            },
+            xaxis_title =" ",
+            yaxis_title = "Esperanza de años",
+            xaxis_rangeslider_visible=False
+        )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="España",
+                        y=df["Spain"],
+                        line = dict(color='black')
+                    )
+            )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Francia",
+                        y=df["France"],
+                        line = dict(color='skyblue')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Alemania",
+                        y=df["Germany"],
+                        line = dict(color='goldenrod')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Italia",
+                        y=df["Italy"],
+                        line = dict(color='red')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Portugal",
+                        y=df["Portugal"],
+                        line = dict(color='green')
+                    )
+
+    )
+        
+    return fige
+
+#Life Expectancy in Asia
+def lifexpa():
+    figa=go.Figure()
+    figa.update_layout(
+            title={
+                "text":"LifeExpectancy",
+                "x" : 0.5,
+                "xanchor":"center"
+            },
+            xaxis_title =" ",
+            yaxis_title = "Esperanza de años",
+            xaxis_rangeslider_visible=False
+        )
+    figa.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="China",
+                        y=df["China"],
+                        line = dict(color='black')
+                    )
+            )
+    figa.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Japón",
+                        y=df["Japan"],
+                        line = dict(color='skyblue')
+                    )
+
+    )
+    figa.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Corea del sur",
+                        y=df["South Korea"],
+                        line = dict(color='goldenrod')
+                    )
+
+    )
+    figa.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="India",
+                        y=df["India"],
+                        line = dict(color='red')
+                    )
+
+    )
+    figa.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Tailandia",
+                        y=df["Thailand"],
+                        line = dict(color='green')
+                    )
+
+    )
+        
+    return figa
+
+#Life Expectancy in Africa
+def lifexpaf():
+    fige=go.Figure()
+    fige.update_layout(
+            title={
+                "text":"LifeExpectancy",
+                "x" : 0.5,
+                "xanchor":"center"
+            },
+            xaxis_title =" ",
+            yaxis_title = "Esperanza de años",
+            xaxis_rangeslider_visible=False
+        )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Egipto",
+                        y=df["Egypt"],
+                        line = dict(color='black')
+                    )
+            )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Argelia",
+                        y=df["Algeria"],
+                        line = dict(color='skyblue')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Alemania",
+                        y=df["Germany"],
+                        line = dict(color='goldenrod')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Nigeria",
+                        y=df["Nigeria"],
+                        line = dict(color='red')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Botsuana",
+                        y=df["Botswana"],
+                        line = dict(color='green')
+                    )
+
+    )
+        
+    return fige
+
+#Life Expectancy in Oceanía
+def lifexpo():
+    fige=go.Figure()
+    fige.update_layout(
+            title={
+                "text":"LifeExpectancy",
+                "x" : 0.5,
+                "xanchor":"center"
+            },
+            xaxis_title =" ",
+            yaxis_title = "Esperanza de años",
+            xaxis_rangeslider_visible=False
+        )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Nueva Zelanda",
+                        y=df["New Zealand"],
+                        line = dict(color='black')
+                    )
+            )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Australia",
+                        y=df["Australia"],
+                        line = dict(color='skyblue')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Fiji",
+                        y=df["Fiji"],
+                        line = dict(color='goldenrod')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Tonga",
+                        y=df["Tonga"],
+                        line = dict(color='red')
+                    )
+
+    )
+    fige.add_trace(
+                    go.Scatter(
+                        x=df["Year"],
+                        name="Islas Salomón",
+                        y=df["Solomon Islands"],
+                        line = dict(color='green')
+                    )
+
+    )
+        
+    return fige
